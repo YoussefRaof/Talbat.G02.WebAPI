eb1aceea28a5c791f0f74cd8132a9e63224a80d1
Ofaa85 <youssefroaf94@gmail.com>
2024-04-09 14:07:53 +0200

    Add project files.

---
+++ Talbat.APIs/Program.cs
@@ -0,0 +1,50 @@
+namespace Talbat.APIs
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var webApplicationBuilder = WebApplication.CreateBuilder(args);
+
+			#region Configure Services
+
+			// Add services to the DI container.
+
+			webApplicationBuilder.Services.AddControllers(); // Register Web API Services In DI Container
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			webApplicationBuilder.Services.AddEndpointsApiExplorer();
+			webApplicationBuilder.Services.AddSwaggerGen(); 
+			#endregion
+
+			var app = webApplicationBuilder.Build();
+
+			#region Configure Kestral Middlewares
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			
+			//app.UseRouting();
+			//app.UseEndpoints(endpoints =>
+			//{
+			//	endpoints.MapControllerRoute(
+
+			//		name: "default",
+			//		pattern: "{controller}/{action}/{id?}"
+			//		);
+			//	endpoints.MapControllers();	
+
+			//});
+
+			app.MapControllers(); 
+			#endregion
+
+			app.Run();
+		}
+	}
+}
