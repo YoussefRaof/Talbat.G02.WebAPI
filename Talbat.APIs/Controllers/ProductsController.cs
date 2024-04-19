b34db7e58d663595d5ea4fa53ede6f935aa00092
Ofaa85 <youssefroaf94@gmail.com>
2024-04-19 18:03:49 +0200

    GetCategories, GetBrands EndPoints

--- Talbat.APIs/Controllers/ProductsController.cs
+++ Talbat.APIs/Controllers/ProductsController.cs
@@ -16,36 +16,59 @@ namespace Talabat.APIs.Controllers
 	{
 		private readonly IGenericRepository<Product> _productsReop;
 		private readonly IMapper _mapper;
+		private readonly IGenericRepository<ProductBrand> _brandRepo;
+		private readonly IGenericRepository<ProductCategory> _categoryRepo;
 
-		public ProductsController(IGenericRepository<Product> productsReop,IMapper mapper)
-        {
+		public ProductsController
+			(IGenericRepository<Product> productsReop, IMapper mapper,
+			IGenericRepository<ProductBrand> brandRepo, IGenericRepository<ProductCategory> categoryRepo)
+		{
 			_productsReop = productsReop;
 			_mapper = mapper;
+			_brandRepo = brandRepo;
+			_categoryRepo = categoryRepo;
 		}
 
 		[HttpGet]
 		public async Task<ActionResult<IEnumerable<ProductToReturnDto>>> GetProducts()
 		{
 			var spec = new ProductWithBrandAndCategorySpecifications();
-			var products= await _productsReop.GetAllWithSpecsAsync(spec);
-			return Ok(_mapper.Map<IEnumerable<Product>,IEnumerable<ProductToReturnDto>>(products));
+			var products = await _productsReop.GetAllWithSpecsAsync(spec);
+			return Ok(_mapper.Map<IEnumerable<Product>, IEnumerable<ProductToReturnDto>>(products));
 		}
 
 		// baseurl/api/products/1
 
-		[ProducesResponseType(typeof(ProductToReturnDto),StatusCodes.Status200OK)]
-		[ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
+		[ProducesResponseType(typeof(ProductToReturnDto), StatusCodes.Status200OK)]
+		[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
 		[HttpGet("{id}")]
 		public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
 		{
-			var spec = new ProductWithBrandAndCategorySpecifications( id);
+			var spec = new ProductWithBrandAndCategorySpecifications(id);
 			var product = await _productsReop.GetWithSpec(spec);
 			if (product is null)
 				return NotFound(new ApiResponse(404)); //404
 
-			return Ok(_mapper.Map<Product,ProductToReturnDto>(product)); //200
+			return Ok(_mapper.Map<Product, ProductToReturnDto>(product)); //200
 		}
 
-			
-    }
+		[HttpGet("brands")]  //GET : /api/products/brands
+
+		public async Task<ActionResult<IEnumerable<ProductBrand>>> GetBrands()
+		{
+			var brands = await _brandRepo.GetAllAsync();
+
+			return Ok(brands);
+		}
+
+		[HttpGet("categories")] //GET: /api/products/categories
+
+		public async Task<ActionResult<IEnumerable<ProductCategory>>> GetCategories()
+		{
+			var categories = await _categoryRepo.GetAllAsync();
+			return Ok(categories);
+		}
+
+
+	}
 }
